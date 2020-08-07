5dc3070d4dcb59e5cbaf4f79dd279f69494bd8af
rabah babaci <4rabah@gmail.com>
2020-08-07 16:12:38 -0700

    implement the merge sort in place

--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -41,9 +41,28 @@ def merge_sort(arr):
 # In other words, your implementation should not allocate any additional lists
 # or data structures; it can only re-use the memory it was given as input
 def merge_in_place(arr, start, mid, end):
-    pass
+    while start < mid and mid <= end:
+        if arr[mid] < arr[start]:
+            # insert mid in start and shift all elements to the right
+            arr.insert(start, arr[mid])
+            arr = arr[: mid + 1] + arr[mid + 2 :]
+            mid += 1
+        start += 1
+    return arr
 
 
 def merge_sort_in_place(arr, l, r):
-    pass
+    if len(arr) <= 1:
+        return arr
+
+    left = arr[: (l + r) // 2]
+    right = arr[(l + r) // 2 :]
+
+    merge_sort_in_place(left, 0, len(left))
+    merge_sort_in_place(right, 0, len(right))
+
+    for i, n in enumerate(merge_in_place(left + right, 0, (l + r) // 2, len(arr) - 1)):
+        arr[i] = n
+
+    return arr
 
