e0c912fd1594eca8026ac47460419c4b5237559e
rabah babaci <4rabah@gmail.com>
2020-08-08 10:58:22 -0700

    update the implemntation of merge sort in place

--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -42,6 +42,8 @@ def merge_sort(arr):
 # or data structures; it can only re-use the memory it was given as input
 def merge_in_place(arr, start, mid, end):
     start2 = mid + 1
+    # if last value in first arr is less then first value in second arr
+    # simply return, values already sorted
     if arr[mid] <= arr[start2]:
         return
 
@@ -49,13 +51,10 @@ def merge_in_place(arr, start, mid, end):
         if arr[start] <= arr[start2]:
             start += 1
         else:
-            value = arr[start2]
-            index = start2
-
-            while index != start:
+            value, index = arr[start2], start2
+            while start != index:
                 arr[index] = arr[index - 1]
                 index -= 1
-
             arr[start] = value
 
             start += 1
@@ -69,7 +68,6 @@ def merge_sort_in_place(arr, l, r):
 
         merge_sort_in_place(arr, l, mid)
         merge_sort_in_place(arr, mid + 1, r)
-
         merge_in_place(arr, l, mid, r)
 
     return arr
